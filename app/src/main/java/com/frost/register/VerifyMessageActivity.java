523feff9e5130982e149834a225d3b2887a21f8e
Samundroids <samundroids@gmail.com>
2017-05-05 19:08:09 +0545

    Added Back Button Feature that works

--- app/src/main/java/com/frost/register/VerifyMessageActivity.java
+++ app/src/main/java/com/frost/register/VerifyMessageActivity.java
@@ -3,14 +3,32 @@ package com.frost.register;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class VerifyMessageActivity extends AppCompatActivity {
+
+    Button btn_verify_page_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_message);
+
+        btn_verify_page_back = (Button) findViewById(R.id.btn_verify_page_back);
+
+        btn_verify_page_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent_back = new Intent(getApplicationContext(), MainActivity.class);
+
+                //This will prevent MainActivity from being destroyed and recreated.
+                // We add "FLAG_ACTIVITY_SINGLE_TOP" to the Intent to go back to the MainActivity
+                intent_back.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent_back);
+            }
+        });
 
         Intent intent = getIntent();
 
